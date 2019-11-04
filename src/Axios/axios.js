160e6ae5cd3029019c003e3e04862e83f57ccb29
mcast045 <54549449+mcast045@users.noreply.github.com>
2019-11-04 14:04:01 -0800

    Update axios

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://rocky-crag-02316.herokuapp.com',
+    baseURL: 'https://rocky-crag-02316.herokuapp.com',
     headers: {
         headerType: 'example header type'
     }
